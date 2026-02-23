b423ba0bb2a64d8e6273d91918edc7ef4ec3de13
hetrechtepadcollege <iestegaar@hetrechtepadcollege.nl>
2026-02-23 12:27:17 +0100

    initial commit naar leer-portaal

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+    
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.style.opacity = '1';
+            card.style.transform = 'translateY(0)';
+        }, index * 200);
+    });
+});
